8622d1f6b42284aed0dc99ad85d72694aff32645
TommyHvidhyld <113069752+TommyHvidhyld@users.noreply.github.com>
2023-01-19 20:41:03 -0600

    render requirements

---
+++ app.py
@@ -0,0 +1,28 @@
+import numpy as np
+from flask import Flask
+
+
+# reflect an existing database into a new model
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/passengers"
+    )
+
+if __name__ == '__main__':
+    app.run()
